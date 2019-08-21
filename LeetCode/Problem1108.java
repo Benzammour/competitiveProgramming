2ecc250f96d6aba0a38ec45aab8ba898a15a3806
benzammour <contact@benzammour.com>
2019-08-21 20:56:58 +0200

    adds Problem 1108 LeetCode

---
+++ LeetCode/Problem1108.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+// Problem 1108: Defanging an IP Address
+public class 1108 {
+
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
